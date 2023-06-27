fda51f2e6eb7ee57bcf3d01e3eff9b087fda2fd2
Naveen-Aroda <lightningnaveen2000@gmail.com>
2023-06-27 11:29:03 +0530

    Added arrows to desktop app

--- app.py
+++ app.py
@@ -39,10 +39,13 @@ class PlotWindow(QtWidgets.QWidget):
         self.canvas.setMinimumSize(1000, 4000)
         self.canvas.setMinimumSize(1001, 4001)
     def excel_to_pandas(self, filename):
-        df_dict = pd.read_excel(filename, sheet_name=['DN', 'UP'])
+        df_dict = pd.read_excel(filename, sheet_name=['DN', 'UP'], header=None)
         down_up = dict()
+        dwn_upp = dict()
         for key, df in df_dict.items():
-            df = df.iloc[2:]
+            first_filled_row_index = df.first_valid_index()
+            df = df.loc[first_filled_row_index:]
+            df = df[2:]
             df = df[:-3]
             df = df.iloc[::-1]
             first_non_empty_row = df.apply(lambda row: row.notnull().any(), axis=1).idxmax()
@@ -62,6 +65,7 @@ class PlotWindow(QtWidgets.QWidget):
             first_column_series = first_column_series.rename(None)
             first_column_series = first_column_series.str.strip()
             df = df.set_index(first_column_series)
+            trains_list = df.columns.tolist()
             list_2d = []
             for column_name in df.columns:
                 column_df = df[column_name]
@@ -83,13 +87,12 @@ class PlotWindow(QtWidgets.QWidget):
                 list_2d = list_2d + [row_indices, datapoints]
             down_up[key] = list_2d
             down_up[key + key] = df.index
-
+            dwn_upp[key] = trains_list
         y_axis = list(dict.fromkeys(down_up['DNDN'].values.tolist()))
         down_up.pop("DNDN")
         down_up.pop("UPUP")
-        return down_up,y_axis
+        return down_up,y_axis, dwn_upp
     def conversion(self, station_dict):
-    # this wil multiply with ratios for plotting
         for key, value in station_dict.items():
             for i in range(len(value)):
                 if i % 2 == 1:  # Check if it's an alternative column
@@ -100,16 +103,15 @@ class PlotWindow(QtWidgets.QWidget):
                         # Convert hours, minutes, and seconds to a decimal representation of hours
                         time_in_hours = hours + (minutes / 60) + (seconds / 3600)
 
-                        # after_decimal = time_in_hours % 1
-                        # rescaled_value = after_decimal * 3/5
-
-                        # time_in_hours = int(time_in_hours) + rescaled_value
+                        after_decimal = time_in_hours % 1
+                        time_in_hours = int(time_in_hours) + after_decimal
 
                         # Update the value in the dictionary
                         value[i][j] = str(round(time_in_hours, 2))
 
                     value[i] = [float(num) for num in value[i]]   
         return station_dict
+
     def add_24_down_up(self, down_up):
         arr_2= down_up['DN']
         for hi in range(1, len(arr_2),2):
@@ -120,23 +122,64 @@ class PlotWindow(QtWidgets.QWidget):
         for h in range(1, len(arr_2),2):
             arr_2[h] = [x + 24 if x < 23 else x for x in arr_2[h]]
         down_up['UP'] = arr_2
+    #     print(down_up)
         return down_up
-    def plot_trains(self,station_dict, y_axis):
+    def plot_trains(self,station_dict, y_axis,trains_dict):
         axes = self.figure.subplots(nrows=4, ncols=1)
+        y_axis.insert(0," ")
+        y_axis.insert(0,"  ")
+        y_axis.insert(0,"   ")
+        y_axis.insert(0,"    ")
+        y_axis.append("     ")
+        y_axis.append("      ")
+        y_axis.append("       ")
+        y_axis.append("        ")
+        print(" ")
+        print("Trains dictionary: ", trains_dict)
+        print(" ")
+    # Arrow:
+    #     eg. axes[2].arrow(20, 25, 0, 1, width = 0.01, head_width=0.1, head_length=0.1, color = 'blue')
+    #     20---> x axis
+    #     25 --> y axis
+    #     1 --> size of line segment
+    
+
+
+    
         for key, arr_2d in station_dict.items():
             for i in range(0, len(arr_2d), 2):
                 for j in range(len(arr_2d[i])):
                     arr_2d[i][j] = y_axis.index(arr_2d[i][j])
         # Subplot 1: 0-8
         axes[0].minorticks_on()
-        xa_0 = np.linspace(0, 8, 240)
+
+        print('station_dict : ', station_dict)
+        # xa_0 = np.linspace(0, 8, 200)
+        xa_0 = np.arange(0, 8, 0.03333)
         for key, arr_2d in station_dict.items():
             for i in range(0, len(arr_2d), 2):
                 axes[0].plot(arr_2d[i+1], arr_2d[i], color='red')
         for i in range(len(y_axis)):
             y_index = y_axis[i]
             ya = [y_index] * len(xa_0)
-            axes[0].plot(xa_0, ya, color='blue', linewidth=1, linestyle=(10, (1, 1.328)))
+            axes[0].scatter(xa_0, ya, marker=',',color='blue', s=0.3)
+            
+        ### ARROW DowN        
+        k = 1    
+        for i in range(len(station_dict['DN']) // 2):    
+            if 0 <= station_dict['DN'][k][0] <= 8:
+                axes[0].text(station_dict['DN'][k][0], station_dict['DN'][k - 1][0] - 2.3, trains_dict['DN'][i], rotation = 'vertical', fontsize=9)
+                axes[0].arrow(station_dict['DN'][k][0], station_dict['DN'][k - 1][0], 0, 1, width = 0.005)
+            k += 2
+        
+        ### ARROW UP        
+        k = 1    
+        for i in range(len(station_dict['UP']) // 2):    
+            if 0 <= station_dict['UP'][k][0] <= 8:
+                axes[0].text(station_dict['UP'][k][0], station_dict['UP'][k - 1][0] + 1.3, trains_dict['UP'][i], rotation = 'vertical', fontsize=9)
+                axes[0].arrow(station_dict['UP'][k][0], station_dict['UP'][k - 1][0], 0, -1, width = 0.005)
+            k += 2     
+        
         axes[0].xaxis.grid(True, which='major', linestyle='-', color='black')
         axes[0].xaxis.grid(True, which='minor', linestyle='-')
         axes[0].xaxis.set_minor_locator(MultipleLocator(10 / 60))
@@ -153,17 +196,34 @@ class PlotWindow(QtWidgets.QWidget):
         axes[0].tick_params(axis='x', which='minor', labelsize=6)
         axes[0].set_xlim(0, 8)
         axes[0].set_ylim(0, len(y_axis))
-
+    
         # Subplot 2: 8-16
         axes[1].minorticks_on()
-        xa_1 = np.linspace(8, 16, 240)
+        xa_1 =np.arange(8, 16, 0.03333)
         for key, arr_2d in station_dict.items():
             for i in range(0, len(arr_2d), 2):
                 axes[1].plot(arr_2d[i+1], arr_2d[i], color='red')
         for i in range(len(y_axis)):
             y_index = y_axis[i]
             ya = [y_index] * len(xa_1)
-            axes[1].plot(xa_1, ya, color='blue', linewidth=1, linestyle=(10, (1, 1.328)))
+            axes[1].scatter(xa_1, ya, marker=',',color='blue', s=0.3)
+            
+        ### ARROW DowN        
+        k = 1    
+        for i in range(len(station_dict['DN']) // 2):    
+            if 8 <= station_dict['DN'][k][0] <= 16:
+                axes[1].text(station_dict['DN'][k][0], station_dict['DN'][k - 1][0] - 2.3, trains_dict['DN'][i], rotation = 'vertical', fontsize=9)
+                axes[1].arrow(station_dict['DN'][k][0], station_dict['DN'][k - 1][0], 0, 1, width = 0.005)
+            k += 2  
+            
+        ### ARROW UP        
+        k = 1    
+        for i in range(len(station_dict['UP']) // 2):    
+            if 8 <= station_dict['UP'][k][0] <= 16:
+                axes[1].text(station_dict['UP'][k][0], station_dict['UP'][k - 1][0] + 1.3, trains_dict['UP'][i], rotation = 'vertical', fontsize=9)
+                axes[1].arrow(station_dict['UP'][k][0], station_dict['UP'][k - 1][0], 0, -1, width = 0.005)
+            k += 2         
+            
         axes[1].xaxis.grid(True, which='major', linestyle='-', color='black')
         axes[1].xaxis.grid(True, which='minor', linestyle='-')
         axes[1].xaxis.set_minor_locator(MultipleLocator(10 / 60))
@@ -182,15 +242,52 @@ class PlotWindow(QtWidgets.QWidget):
         axes[1].set_ylim(0, len(y_axis))
 
         # Subplot 3: 16-24
+        
         axes[2].minorticks_on()
-        xa_2 = np.linspace(16, 24, 240)
+        xa_2 = np.arange(16, 24, 0.03333)
         for key, arr_2d in station_dict.items():
             for i in range(0, len(arr_2d), 2):
                 axes[2].plot(arr_2d[i+1], arr_2d[i], color='red')
         for i in range(len(y_axis)):
             y_index = y_axis[i]
             ya = [y_index] * len(xa_2)
-            axes[2].plot(xa_2, ya, color='blue', linewidth=1, linestyle=(10, (1, 1.328)))
+            axes[2].scatter(xa_2, ya, marker=',',color='blue', s=0.3)
+
+        ### ARROW DowN         
+        k = 1  
+        arrow_lis = []
+        for i in range(len(station_dict['DN']) // 2):    
+            if 16 <= station_dict['DN'][k][0] <= 24:
+                arrow_lis.append(station_dict['DN'][k][0])
+                count = arrow_lis.count(station_dict['DN'][k][0])
+                print(arrow_lis)
+                
+
+        ### ARROW UP   
+        k = 1    
+        for i in range(len(station_dict['UP']) // 2):    
+            if 16 <= station_dict['UP'][k][0] <= 24:
+                axes[2].text(station_dict['UP'][k][0], station_dict['UP'][k - 1][0] + 1.3, trains_dict['UP'][i], rotation = 'vertical', fontsize=9)
+                axes[2].arrow(station_dict['UP'][k][0], station_dict['UP'][k - 1][0] + 1, 0, -1, width = 0.005)
+            k += 2    
+        
+        fontsize = 7
+        offset = 2.3
+
+        k = 1
+        texts = []
+        i = 0
+        for _ in trains_dict['DN']:
+            axes[2].arrow(station_dict['DN'][k][0], station_dict['DN'][k - 1][0] - 1, 0, 1, width = 0.005)
+            texts.append(axes[2].text(station_dict['DN'][k][0], station_dict['DN'][k - 1][0] - offset, trains_dict['DN'][i], rotation='vertical', fontsize=fontsize))
+            
+            x_positions = [station_dict['DN'][k][0], station_dict['DN'][k][0]]
+            y_positions = [station_dict['DN'][k - 1][0] - offset, station_dict['DN'][k - 1][0] - offset - 0.5]        
+            k += 2
+            i += 1
+    
+        adjust_text(texts, ax=axes[2], expand=(0.5, 0.5), time_lim=1, force_text=(1,0))
+        
         axes[2].xaxis.grid(True, which='major', linestyle='-', color='black')
         axes[2].xaxis.grid(True, which='minor', linestyle='-')
         axes[2].xaxis.set_minor_locator(MultipleLocator(10 / 60))
@@ -208,16 +305,36 @@ class PlotWindow(QtWidgets.QWidget):
         axes[2].set_xlim(16, 24)
         axes[2].set_ylim(0, len(y_axis))
         
-        # Subplot 3: 16-24
+        
+        # Subplot 4: 24-31
+        
         axes[3].minorticks_on()
-        xa_3 = np.linspace(24, 32, 240)
+        xa_3 = np.arange(24, 32, 0.03333)
         for key, arr_2d in station_dict.items():
             for i in range(0, len(arr_2d), 2):
                 axes[3].plot(arr_2d[i+1], arr_2d[i], color='red')
         for i in range(len(y_axis)):
             y_index = y_axis[i]
             ya = [y_index] * len(xa_3)
-            axes[3].plot(xa_3, ya, color='blue', linewidth=1, linestyle=(10, (1, 1.328)))
+            axes[3].scatter(xa_3, ya, marker=',',color='blue', s=0.3)
+            
+            
+        ### ARROW DowN       
+        k = 1    
+        for i in range(len(station_dict['DN']) // 2):    
+            if 24 <= station_dict['DN'][k][0] <= 32:
+                axes[3].text(station_dict['DN'][k][0], station_dict['DN'][k - 1][0] - 2.3, trains_dict['DN'][i], rotation = 'vertical', fontsize=9)
+                axes[3].arrow(station_dict['DN'][k][0], station_dict['DN'][k - 1][0] - 1, 0, 1, width = 0.005)
+            k += 2
+            
+        ### ARROW UP        
+        k = 1    
+        for i in range(len(station_dict['UP']) // 2):    
+            if 24 <= station_dict['UP'][k][0] <= 32:
+                axes[3].text(station_dict['UP'][k][0], station_dict['UP'][k - 1][0] + 1.3, trains_dict['UP'][i], rotation = 'vertical', fontsize=9)
+                axes[3].arrow(station_dict['UP'][k][0], station_dict['UP'][k - 1][0] + 1, 0, -1, width = 0.005)
+            k += 2     
+            
         axes[3].xaxis.grid(True, which='major', linestyle='-', color='black')
         axes[3].xaxis.grid(True, which='minor', linestyle='-')
         axes[3].xaxis.set_minor_locator(MultipleLocator(10 / 60))
@@ -236,6 +353,10 @@ class PlotWindow(QtWidgets.QWidget):
         axes[3].set_ylim(0, len(y_axis))
         
         plt.tight_layout()
+    #     for me, ax in enumerate(axes):
+    #         ax.set_title(f"Subplot {me+1}")
+    #         # Save each subplot as a separate PDF
+    #         plt.savefig(f"subplot_{me+1}.pdf")
         self.canvas.draw()
 
     #     for me, ax in enumerate(axes):
@@ -249,13 +370,13 @@ class PlotWindow(QtWidgets.QWidget):
         file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open Excel File", "", "Excel Files (*.xlsx)")
         if file_name:
             # data = pd.read_excel(file_name)
-            down_up, y_labes =  self.excel_to_pandas(file_name)
+            down_up, y_labes,dwn_upp =  self.excel_to_pandas(file_name)
             down_up = self.conversion(down_up)
             down_up = self.add_24_down_up(down_up)
             # x = data["x"]
             # y = data["y"]             
             self.figure.clear()
-            self.plot_trains(down_up, y_labes)
+            self.plot_trains(down_up, y_labes, dwn_upp)
             # ax = self.figure.add_subplot(111)
             # ax.plot(x, y, "r-")
             # ax.set_xlabel("x")
