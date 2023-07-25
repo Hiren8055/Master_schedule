d046556898390759c7d42f1597d6c00369041457
Hiren8055 <73933595+Hiren8055@users.noreply.github.com>
2023-07-25 23:04:25 +0530

    Removed blank space in station_dict

--- plot.py
+++ plot.py
@@ -6,17 +6,6 @@ from adjustText import adjust_text
 import matplotlib.transforms as mtransforms
 
 def plot_trains(station_dict, y_axis, y_labes,trains_dict):
-    y_axis.insert(0," ")
-    y_axis.insert(0,"  ")
-    y_axis.insert(0,"   ")
-    y_axis.insert(0,"    ")
-    y_axis.append("     ")
-    y_axis.append("      ")
-    y_axis.append("       ")
-    y_axis.append("        ")
-    #print(" ")
-    # #print("Trains dictionary: ", trains_dict)
-    #print(" ")
 # Arrow:
 #     eg. axes[2].arrow(20, 25, 0, 1, width = 0.01, head_width=0.1, head_length=0.1, color = 'blue')
 #     20---> x axis
@@ -33,9 +22,6 @@ def plot_trains(station_dict, y_axis, y_labes,trains_dict):
         for i in range(0, len(arr_2d), 2):
             for j in range(len(arr_2d[i])):
                 arr_2d[i][j] = y_axis.index(arr_2d[i][j])
-    def Reverse(lst):
-        new_lst = lst[::-1]
-        return new_lst
 
     # Subplot 1: 0-8
     def plot(index_0,index_1,arr,start_sub_y_axis,end_sub_y_axis,ylim_start,ylim_end,xlim_start,xlim_end):
@@ -62,9 +48,8 @@ def plot_trains(station_dict, y_axis, y_labes,trains_dict):
         axes[index_0][index_1].xaxis.set_minor_locator(MultipleLocator(10 / 60))
         axes[index_0][index_1].set_xticks(arr)
         axes[index_0][index_1].set_xticklabels(arr)
-        
         sub_y_axis = y_axis[start_sub_y_axis:end_sub_y_axis]
-        sub_y_axis = Reverse(sub_y_axis)
+        # sub_y_axis = Reverse(sub_y_axis)
         print(sub_y_axis)
         axes[index_0][index_1].set_ylim(start_sub_y_axis,end_sub_y_axis)
         print(start_sub_y_axis,end_sub_y_axis)
@@ -83,22 +68,27 @@ def plot_trains(station_dict, y_axis, y_labes,trains_dict):
         axes[index_0][index_1].xaxis.set_minor_formatter(formatter)
         axes[index_0][index_1].tick_params(axis='x', which='minor', labelsize=6)
         axes[index_0][index_1].set_xlim(xlim_start,xlim_end)  
+        axes[index_0][index_1].invert_yaxis()
+
+
+
+
     arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     arr2 = [8, 9, 10, 11, 12, 13, 14, 15, 16]
     arr3 = [16, 17, 18, 19, 20, 21, 22, 23, 24]
     
 #   (index_0,index_1,arr,start_sub_y_axis,end_sub_y_axis,ylim_start,ylim_end,xlim_start,xlim_end)
-    plot(0,0,arr1,4,15,      0,15,        0,8)
-    plot(1,0,arr1,14,35,    0,19,        0,8)   
-    plot(2,0,arr1,34,49,    0,19,        0,8) 
+    plot(0,0,arr1,0,11,      0,15,        0,8)
+    plot(1,0,arr1,10,31,    0,19,        0,8)   
+    plot(2,0,arr1,30,45,    0,19,        0,8) 
 
-    plot(0,1,arr2,4,15,     0,15,        8,16)
-    plot(1,1,arr2,14,35,   0,19,        8,16)   
-    plot(2,1,arr2,34,49,   0,19,        8,16)
+    plot(0,1,arr2,0,11,     0,15,        8,16)
+    plot(1,1,arr2,10,31,  0,19,        8,16)   
+    plot(2,1,arr2,30,45,   0,19,        8,16)
 
-    plot(0,2,arr3,4,15,     0,15,       16,24)
-    plot(1,2,arr3,14,35,   0,19,       16,24)   
-    plot(2,2,arr3,34,49,   0,19,       16,24)
+    plot(0,2,arr3,0,11,     0,15,       16,24)
+    plot(1,2,arr3,10,31,   0,19,       16,24)   
+    plot(2,2,arr3,30,45,   0,19,       16,24)
     
          
     plt.tight_layout()
@@ -107,65 +97,65 @@ def plot_trains(station_dict, y_axis, y_labes,trains_dict):
 #         # Save each subplot as a separate PDF
 #         plt.savefig(f"subplot_{me+1}.pdf")
     buf = 0.001
-    # fig.savefig(
-    # "frac00.pdf",
-    # bbox_inches = mtransforms.Bbox([[0, 0.666], [0.335,1]]).transformed( # [[xmin, ymin], [xmax, ymax]]
-    #     fig.transFigure - fig.dpi_scale_trans
-    # ),format="pdf")
-    # fig.savefig(
-    # "frac01.pdf",
-    # bbox_inches = mtransforms.Bbox([[0.335 - buf, 0.666], [0.667,1]]).transformed( # [[xmin, ymin], [xmax, ymax]]
-    #     fig.transFigure - fig.dpi_scale_trans
-    # ),format="pdf")
-    # fig.savefig(
-    # "frac02.pdf",
-    # bbox_inches = mtransforms.Bbox([[0.667, 0.666], [1.002,1]]).transformed( # [[xmin, ymin], [xmax, ymax]]
-    #     fig.transFigure - fig.dpi_scale_trans
-    # ),format="pdf")
+    fig.savefig(
+    "frac00.pdf",
+    bbox_inches = mtransforms.Bbox([[0, 0.666], [0.335,1]]).transformed( # [[xmin, ymin], [xmax, ymax]]
+        fig.transFigure - fig.dpi_scale_trans
+    ),format="pdf")
+    fig.savefig(
+    "frac01.pdf",
+    bbox_inches = mtransforms.Bbox([[0.335 - buf, 0.666], [0.667,1]]).transformed( # [[xmin, ymin], [xmax, ymax]]
+        fig.transFigure - fig.dpi_scale_trans
+    ),format="pdf")
+    fig.savefig(
+    "frac02.pdf",
+    bbox_inches = mtransforms.Bbox([[0.667, 0.666], [1.002,1]]).transformed( # [[xmin, ymin], [xmax, ymax]]
+        fig.transFigure - fig.dpi_scale_trans
+    ),format="pdf")
+
+
+
+
+    fig.savefig(
+    "frac10.pdf",
+    bbox_inches=mtransforms.Bbox([[0, 0.34 ], [0.335, 0.666 ]]).transformed( # [[xmin, ymin], [xmax, ymax]]
+        fig.transFigure - fig.dpi_scale_trans
+    ),format="pdf")
+    fig.savefig(
+    "frac11.pdf",
+    bbox_inches=mtransforms.Bbox([[0.335 - buf, 0.34], [0.667, 0.666]]).transformed( # [[xmin, ymin], [xmax, ymax]]
+        fig.transFigure - fig.dpi_scale_trans
+    ),format="pdf")
+    fig.savefig(
+    "frac12.pdf",
+    bbox_inches=mtransforms.Bbox([[0.667, 0.34 ], [1.002, 0.666 ]]).transformed( # [[xmin, ymin], [xmax, ymax]]
+        fig.transFigure - fig.dpi_scale_trans
+    ),format="pdf")
 
 
 
 
     # fig.savefig(
-    # "frac10.pdf",
-    # bbox_inches=mtransforms.Bbox([[0, 0.34 ], [0.335, 0.666 ]]).transformed( # [[xmin, ymin], [xmax, ymax]]
-    #     fig.transFigure - fig.dpi_scale_trans
-    # ),format="pdf")
-    # fig.savefig(
-    # "frac11.pdf",
-    # bbox_inches=mtransforms.Bbox([[0.335 - buf, 0.34], [0.667, 0.666]]).transformed( # [[xmin, ymin], [xmax, ymax]]
-    #     fig.transFigure - fig.dpi_scale_trans
-    # ),format="pdf")
-    # fig.savefig(
-    # "frac12.pdf",
-    # bbox_inches=mtransforms.Bbox([[0.667, 0.34 ], [1.002, 0.666 ]]).transformed( # [[xmin, ymin], [xmax, ymax]]
+    # "frac2.pdf",
+    # bbox_inches=mtransforms.Bbox([[0, 0.25], [1, 0.5]]).transformed(
     #     fig.transFigure - fig.dpi_scale_trans
     # ),format="pdf")
 
-
-
-
-    # # fig.savefig(
-    # # "frac2.pdf",
-    # # bbox_inches=mtransforms.Bbox([[0, 0.25], [1, 0.5]]).transformed(
-    # #     fig.transFigure - fig.dpi_scale_trans
-    # # ),format="pdf")
-
-    # fig.savefig(
-    # "frac20.pdf",
-    # bbox_inches=mtransforms.Bbox([[0, 0], [0.335, 0.34-3*buf]]).transformed( # [[xmin, ymin], [xmax, ymax]]
-    #     fig.transFigure - fig.dpi_scale_trans
-    # ),format="pdf")
-    # fig.savefig(
-    # "frac21.pdf",
-    # bbox_inches=mtransforms.Bbox([[0.335 - buf, 0], [0.667, 0.34-3*buf]]).transformed( # [[xmin, ymin], [xmax, ymax]]
-    #     fig.transFigure - fig.dpi_scale_trans
-    # ),format="pdf")
-    # fig.savefig(
-    # "frac22.pdf",
-    # bbox_inches=mtransforms.Bbox([[0.667, 0], [1.002, 0.34-3*buf]]).transformed( # [[xmin, ymin], [xmax, ymax]]
-    #     fig.transFigure - fig.dpi_scale_trans
-    # ),format="pdf")
+    fig.savefig(
+    "frac20.pdf",
+    bbox_inches=mtransforms.Bbox([[0, 0], [0.335, 0.34-3*buf]]).transformed( # [[xmin, ymin], [xmax, ymax]]
+        fig.transFigure - fig.dpi_scale_trans
+    ),format="pdf")
+    fig.savefig(
+    "frac21.pdf",
+    bbox_inches=mtransforms.Bbox([[0.335 - buf, 0], [0.667, 0.34-3*buf]]).transformed( # [[xmin, ymin], [xmax, ymax]]
+        fig.transFigure - fig.dpi_scale_trans
+    ),format="pdf")
+    fig.savefig(
+    "frac22.pdf",
+    bbox_inches=mtransforms.Bbox([[0.667, 0], [1.002, 0.34-3*buf]]).transformed( # [[xmin, ymin], [xmax, ymax]]
+        fig.transFigure - fig.dpi_scale_trans
+    ),format="pdf")
 
 
     #print("saved-------------------------")
@@ -180,3 +170,6 @@ def plot_trains(station_dict, y_axis, y_labes,trains_dict):
                     wspace=0.4,
                     hspace=0.4)
     plt.show()
+
+
+
