ad158e1538a2041dcd3ae7426d70694d134d4e63
amishra9919 <86140544+amishra9919@users.noreply.github.com>
2023-08-23 13:24:39 -0700

    Update collide_labels.py
    again converted into 2dim

--- collide_labels.py
+++ collide_labels.py
@@ -5,54 +5,70 @@ def is_sorted_ascending(lst):
     return lst == sorted(lst)
 
 def add_arrow_labels(x, y):
-    inx = 0   #NOTE: not necessary
+    inx, iny = 0, 0   #NOTE: not necessary
     # for 0, 0
     if (0 <= x < 8 and 0 <= y <= 11 ) :
-        inx =   0
+        # print("condition triggered for label")
+        inx = 0;iny = 0  
     elif (24 <= x <= 32 and 0 <= y <= 11):
+        # print("condition triggered for label MINUSING")
         x = x - 24
+        # inx = 0;iny = 0
     # for 0, 1
     elif (8 <= x < 16 and 0 <= y <= 11) :
-        inx = 1
+        # print("condition triggered for label")
+        inx = 0;iny = 1
     # for 0, 2
     elif (16 <= x <= 24 and 0 <= y <= 11) :
-        inx = 2
+        # print("condition triggered for label")
+        inx = 0;iny = 2
     # for 1, 0
     elif (0 <= x < 8 and 11 < y <= 31) :
-        inx = 3
+        # print("condition triggered for label")
+        inx = 1;iny = 0
     elif (24 <= x <= 32 and 11 < y <= 31):
+        # print("condition triggered for label MINUSING")
         x = x - 24
-        inx = 3
+        inx = 1;iny = 0
     # for 1, 1
     elif (8 <= x < 16 and 11 < y <= 31) :
-        inx = 4
+        # print("condition triggered for label")
+        inx = 1;iny = 1
     # for 1, 2
     elif (16 <= x <= 24 and 11 < y <= 31) :
-        inx = 5
+        # print("condition triggered for label")
+        inx = 1;iny = 2
     # for 2, 0
     elif (0 <= x < 8 and 31 < y <= 45) :
-        inx = 6
+        # print("condition triggered for label")
+        inx = 2;iny = 0
     elif (24 <= x <= 32 and 31 < y <= 45):
+        # print("condition triggered for label MINUSING")
         x = x - 24
-        inx = 6
+        inx = 2;iny = 0
     # for 2, 1
     elif (8 <= x < 16 and 31 < y <= 45) :
-        inx = 7
+        # print("condition triggered for label")
+        inx = 2;iny = 1
     # for 2, 2
     elif (16 <= x <= 24 and 31 < y <= 45) :
-        inx =  8
-    return inx, x, y
+        # print("condition triggered for label")
+        inx = 2;iny = 2 
+    return inx, iny, x, y
 
 def collision_text_updn(collision_merged, axes):   
+    # def collision_text_updn(collision_updn,  new_dict):   
         """ this function takes cares in overlapping of labels"""
 
         """ Variables Declaration"""
         k = 1
+        last_y = 0
+
         previous_x, previous_y= 0, 0
         label_var = ''
         y_overlap_both_up = 0
         y_buffer_both_up = 0
-
+        # original_x = 0
 
         for i in range(len(collision_merged[0])):
             
@@ -60,23 +76,52 @@ def collision_text_updn(collision_merged, axes):
             x = collision_merged[1][i]
             y = collision_merged[2][i]
 
+            range_of_x = x + 0.12            # 0.7 is x size of labels
+            range_of_y = y + 0.9
+
+            # if i == 0:                       # NOTE: THIS IS REQ
+            #     overlap_increment = i
+
+            # if len(dup_x[dup_x < range_of_x]) == 0 or len(dup_y[dup_y < range_of_y]) == 0:     # NOTE: IF IS REQUIRED
+            #     if y == last_y :
+            #         # axes[2].text(x + overlap_increment, y - 2.5, trains_dict['DN'][i], rotation = 'vertical', fontsize=9)
+            #         axes[index_0][index_1].text(x + overlap_increment, y + 1, new_dict['UP'][k - 1], rotation = 'vertical', fontsize=9)
+            #         # print("I am in FIRST shifting plot ", new_dict['DN'][k - 1], x)    
+            #     else:
+            #     #     ## normal text
+            #         axes[index_0][index_1].text(x, y + 1, new_dict['UP'][k - 1], rotation = 'vertical', fontsize=9) 
+            #         overlap_increment = 0   
+            #         # print("I am in normal plot ", new_dict['DN'][k - 1], x)
+            # else:              
+            #     ## perform shifting
+            #     axes[index_0][index_1].text(x + overlap_increment, y + 1, new_dict['UP'][k - 1], rotation = 'vertical', fontsize=9) 
+            #     # print("I am in shifting plot ", new_dict['DN'][k - 1], x)                
+            #     overlap_increment += 0.12   
+            #     last_y = y
+            # print("this",x, y + 1, new_dict['UP'][k - 1])               # NOTE: INSIDE IF
+            # axes[0][0].text(x, y + 1, new_dict['UP'][k - 1], rotation = 'vertical', fontsize=9) 
+
+            # print(previous_x, x, previous_y, y)
             if abs(x - previous_x) <= 0.03 and y == previous_y:
+                # print('label is overlapping')
                 label_var = '/'
                 len_of_labels = len(label_)
                 y_buffer_both_up = y_buffer_both_up + (0.06 * len_of_labels)
                 """y_overlap_both_up is y axis for text which is different in arrows y axis that is original 'y' """
                 y_overlap_both_up = y + y_buffer_both_up 
             else:
+                # print('label is NOT overlapping')
                 label_var = ''
                 y_buffer_both_up = 0
                 y_overlap_both_up = y + y_buffer_both_up                         
 
+            # label = new_dict['UPDN'][k - 1] + label_var
             label = label_ + label_var
 
             # original_x = x
-            inx, x, y = add_arrow_labels(x, y)
-            axes[inx].text(x - 0.02, y_overlap_both_up + 0.8, label, rotation = 'vertical', fontsize=13)  # NOTE: INSIDE IF
-            axes[inx].arrow(x, y, 0, 0.5, width = 0.005, clip_on = False)
+            inx, iny, x, y = add_arrow_labels(x, y)
+            axes[inx][iny].text(x - 0.02, y_overlap_both_up + 0.8, label, rotation = 'vertical', fontsize=13)  # NOTE: INSIDE IF
+            axes[inx][iny].arrow(x, y, 0, 0.5, width = 0.005, clip_on = False)
 
             previous_x, previous_y = x, y
             k += 3
@@ -94,6 +139,7 @@ def collision_text_updn1(collision_merged1, axes):
         label_var = ''
         y_overlap_both_up = 0
         y_buffer_both_up = 0
+        # original_x = 0
 
         for i in range(len(collision_merged1[0])):
             
@@ -101,6 +147,7 @@ def collision_text_updn1(collision_merged1, axes):
             x = collision_merged1[1][i]
             y = collision_merged1[2][i]
 
+            # print(previous_x, x, previous_y, y)
             if abs(x - previous_x) <= 0.03 and y == previous_y:
                 # print('label is overlapping')
                 label_var = '/'
@@ -109,17 +156,21 @@ def collision_text_updn1(collision_merged1, axes):
                 """y_overlap_both_up is y axis for text which is different in arrows y axis that is original 'y' """
                 y_overlap_both_up = y - y_buffer_both_up 
             else:
+                # print('label is NOT overlapping')
                 label_var = ''
                 y_buffer_both_up = 0
                 y_overlap_both_up = y - y_buffer_both_up                         
 
+            # label = new_dict['UPDN'][k - 1] + label_var
             label = label_var + label_
 
-            inx, x, y = add_arrow_labels(x, y)
-            axes[inx].text(x - 0.02, y_overlap_both_up - 0.55, label, rotation = 'vertical', fontsize=13)  # NOTE: INSIDE IF
-            axes[inx].arrow(x, y, 0, - 0.5, width = 0.005, clip_on = False)
+            # original_x = x
+            inx, iny, x, y = add_arrow_labels(x, y)
+            axes[inx][iny].text(x - 0.02, y_overlap_both_up - 0.55, label, rotation = 'vertical', fontsize=13)  # NOTE: INSIDE IF
+            axes[inx][iny].arrow(x, y, 0, - 0.5, width = 0.005, clip_on = False)
 
             previous_x, previous_y = x, y
+
 
             ###############################################################################
             k += 3
