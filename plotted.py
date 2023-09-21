e55ffa6d7da9a4b007bbce17ff6420159a2ad193
Naveen-Aroda <84771286+Naveen-Aroda@users.noreply.github.com>
2023-09-21 22:21:34 +0530

    Plotted UP & DN rectangles and intersecting labels.

--- plotted.py
+++ plotted.py
@@ -1,8 +1,8 @@
-#changes - function comment
 from PySide2 import QtWidgets
 from PySide2.QtGui import QFont
 from PySide2.QtWidgets import QScrollArea, QMessageBox, QApplication
 from matplotlib.figure import Figure
+from matplotlib import patches
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
@@ -16,10 +16,11 @@ from matplotlib.ticker import FixedFormatter
 import re
 import copy
 from collections import Counter
-from preprocessing import intersection
-
 from labels import *
+from intersection import intersection
+
 class plotted_():
+    intersection = intersection
     def __init__(self, figure, y_axis, y_labes,canvas, layout,export_button,axes, scroll_area, toolbar) -> None:
         # self.excel_to_pandas = excel_to_pandas
         # self.select = select
@@ -43,27 +44,43 @@ class plotted_():
         self.axes = axes
         self.scroll_area = scroll_area
         self.toolbar = toolbar
+        self.artist_list = []
 
-
-
-
-    def plot(self,index_0,index_1,arr,start_sub_y_axis,end_sub_y_axis,ylim_start,ylim_end,xlim_start,xlim_end, station_dict):
+    def plot(self,index_0,index_1,arr,start_sub_y_axis,end_sub_y_axis,ylim_start,ylim_end,xlim_start,xlim_end, color_dict, station_dict, rect_dict):
         self.axes[index_0][index_1].minorticks_on()
-
         # xa_0 = np.linspace(0, 8, 200)
         xa_0 = np.arange(xlim_start, xlim_end, 0.03333)
-        for key, arr_2d in station_dict.items():
+        for arr_2d, color_list in zip(station_dict.values(), color_dict.values()):
+            self.canvas.flush_events()
             for i in range(0, len(arr_2d), 2):
                 arr_minus = np.array(arr_2d[i+1])
+                self.canvas.flush_events()
                 arr_minus = arr_minus - 24
-                self.axes[index_0][index_1].plot(arr_minus, arr_2d[i], color='red')
-                self.axes[index_0][index_1].plot(arr_2d[i+1], arr_2d[i], color='red')
-
+                line_color = color_list[int(i/2)]
+                (ln1,) = self.axes[index_0][index_1].plot(arr_minus, arr_2d[i], color=line_color)
+                (ln2,) = self.axes[index_0][index_1].plot(arr_2d[i+1], arr_2d[i], color=line_color)
+                self.artist_list.append(ln1)
+                self.artist_list.append(ln2)
+                self.canvas.flush_events()
         for i in range(len(self.y_axis)):
             y_index = self.y_axis[i]
+            self.canvas.flush_events()
             ya = [y_index] * len(xa_0)
-            self.axes[index_0][index_1].scatter(xa_0, ya, marker=',',color='blue', s=0.3)
+            self.artist_list.append(self.axes[index_0][index_1].scatter(xa_0, ya, marker=',',color='blue', s=0.3))
+            self.canvas.flush_events()
 
+        for key, box_list in rect_dict.items():
+            if key =="DN":
+                grid = "-----"
+            elif key =="UP":
+                grid = "|||"
+            for station, start_time, end_time in box_list:
+                xmin = start_time
+                xmax = end_time
+                ymax = station - 0.15
+                ymin = station - 0.85
+                rect = patches.Rectangle((xmin, ymin), width=xmax-xmin, height= ymax-ymin, alpha=0.6, edgecolor='m', hatch=grid, facecolor='none')
+                self.artist_list.append(self.axes[index_0][index_1].add_patch(rect))
 
         self.axes[index_0][index_1].xaxis.grid(True, which='major', linestyle='-', color='black')
         self.axes[index_0][index_1].xaxis.grid(True, which='minor', linestyle='-')
@@ -79,28 +96,46 @@ class plotted_():
         # self.axes[index_0][index_1].set_yticks(range(start_sub_y_axis,end_sub_y_axis))
         self.axes[index_0][index_1].set_yticks(range(start_sub_y_axis,end_sub_y_axis))
         self.axes[index_0][index_1].set_yticklabels(sub_y_axis)
+        self.canvas.flush_events()
 
 
         self.axes[index_0][index_1].tick_params(axis='x', which='minor', labelbottom=True)
         self.axes[index_0][index_1].tick_params(labeltop=True, labelright=True)
         minor_labels = ["10", "20", "30", "40", "50"] * 8
         minor_labels.insert(0, "")
+        self.canvas.flush_events()
+
 
         formatter = FixedFormatter(minor_labels)
         self.axes[index_0][index_1].xaxis.set_minor_formatter(formatter)
         self.axes[index_0][index_1].tick_params(axis='x', which='minor', labelsize=6)
         self.axes[index_0][index_1].set_xlim(xlim_start,xlim_end)  
         self.axes[index_0][index_1].invert_yaxis()
+        self.canvas.flush_events()
 
-    def plot_trains(self, station_dict, trains_dict):
+
+    def plot_trains(self, station_dict, trains_dict, color_dict, rect_dict):
         self.axes = self.figure.subplots(nrows=3, ncols=3)
         # have to make  3 x 3 grid
         # set width of each subplot as 8
         for key, arr_2d in station_dict.items():
+            self.canvas.flush_events()
+            
             for i in range(0, len(arr_2d), 2):
+                self.canvas.flush_events()
+
                 for j in range(len(arr_2d[i])):
                     arr_2d[i][j] = self.y_axis.index(arr_2d[i][j])
-        dup_train_dict = copy.deepcopy(trains_dict)
+                    self.canvas.flush_events()
+
+        # dup_train_dict = copy.deepcopy(trains_dict)
+        self.canvas.flush_events()
+
+
+        for key, box_list in rect_dict.items():
+            for i, (station, *_) in enumerate(box_list):
+                station = self.y_axis.index(station)
+                box_list[i] = station,*_,
 
     ######################################################################################################    
         arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8]
@@ -108,19 +143,20 @@ class plotted_():
         arr3 = [16, 17, 18, 19, 20, 21, 22, 23, 24]
         
     #   (index_0, index_1, arr, start_sub_y_axis, end_sub_y_axis, ylim_start, ylim_end, xlim_start, xlim_end)
-        self.plot(0,0,arr1,0,11,      0,15,        0,8, station_dict)
-        self.plot(1,0,arr1,10,31,    0,19,        0,8, station_dict)   
-        self.plot(2,0,arr1,30,45,    0,19,        0,8, station_dict) 
+        self.plot(0,0,arr1,0,30,      0,15,        0,8, color_dict, station_dict, rect_dict)
+        self.plot(1,0,arr1,29,50,    0,19,        0,8, color_dict, station_dict, rect_dict)   
+        self.plot(2,0,arr1,49,64,    0,19,        0,8, color_dict, station_dict, rect_dict) 
 
-        self.plot(0,1,arr2,0,11,     0,15,        8,16,  station_dict)
-        self.plot(1,1,arr2,10,31,  0,19,        8,16,  station_dict)   
-        self.plot(2,1,arr2,30,45,   0,19,        8,16,  station_dict)
+        self.plot(0,1,arr2,0,30,      0,15,        8,16, color_dict,   station_dict, rect_dict)
+        self.plot(1,1,arr2,29,50,    0,19,       8,16,  color_dict,  station_dict, rect_dict)
+        self.plot(2,1,arr2,49,64,    0,19,       8,16,  color_dict,  station_dict, rect_dict)
 
-        self.plot(0,2,arr3,0,11,     0,15,       16,24,  station_dict)
-        self.plot(1,2,arr3,10,31,   0,19,       16,24,  station_dict)   
-        self.plot(2,2,arr3,30,45,   0,19,       16,24,  station_dict)
+        self.plot(0,2,arr3,0,30,      0,15,       16,24,  color_dict,  station_dict, rect_dict)
+        self.plot(1,2,arr3,29,50,    0,19,       16,24,  color_dict,  station_dict, rect_dict)
+        self.plot(2,2,arr3,49,64,    0,19,        16,24,  color_dict,  station_dict, rect_dict)
 
         def plot_labels():
+            # print("station_dict",len(station_dict["UP"]))
             new_dict = copy.deepcopy(station_dict)
 
             """ Called a func"""
@@ -144,33 +180,29 @@ class plotted_():
             """ Called a func"""        
             collision_merged1 = self.merging_dn_fist_and_up_last_element(collision_dn, collision_up1)
 
-########################################## collision text for up and down ############################################
-            print('print collision merged us de: ', collision_merged)
-            print()
-            print('print collision merged ue ds: ', collision_merged1)
-
+        ########################################## collision text for up and down #################################################\
+            # intersection(station_dict, axes, trains_dict)
+            
             def collision_text_updn1(collision_merged1):
-                """ this function takes care of labels up-end and down-start """
+                """ this function takes care of labels up-end and down-start labels and arrows"""
+                self.canvas.flush_events()
+                """ Variables Declaration"""
                 k = 1
                 previous_x, previous_y= 0, 0
                 label_var = ''
                 y_overlap_both_up = 0
                 y_buffer_both_up = 0
-                # original_x = 0
 
                 for i in range(len(collision_merged1[0])):
+                    self.canvas.flush_events()
 
                     label_ = collision_merged1[0][i]
                     x = collision_merged1[1][i]
                     y = collision_merged1[2][i]
-
-                    if abs(x - previous_x) <= 0.06 and y == previous_y:
-                        # print('label is overlapping')
+                    if abs(x - previous_x) <=0.03 and y == previous_y:
                         label_var = '/'
                         len_of_labels = len(label_)
                         y_buffer_both_up = y_buffer_both_up + (extract_current_axes_ue_ds(x, y) * len_of_labels) 
-                        # y_buffer_both_up = y_buffer_both_up + (0.07 * len_of_labels) + 1
-                        """y_overlap_both_up is y axis for text which is different in arrows y axis that is original 'y' """
                         y_overlap_both_up = y - y_buffer_both_up 
                     else:
                         label_var = ''
@@ -180,53 +212,49 @@ class plotted_():
                     label = label_var + label_
 
                     inx, iny, x, y = self.add_arrow_labels(x, y)
-                    self.axes[inx][iny].text(x - 0.03, y_overlap_both_up - 0.6, label, rotation = 'vertical', fontsize=8) 
-                    self.axes[inx][iny].arrow(x, y, 0, - 0.5, width = 0.005, clip_on = False)
-
+                    self.artist_list.append(self.axes[inx][iny].text(x - 0.02, y_overlap_both_up - 0.5, label, rotation = 'vertical', fontsize=8,picker=True))
+                    self.artist_list.append(self.axes[inx][iny].arrow(x, y, 0, - 0.5, width = 0.005, clip_on = False))
                     previous_x, previous_y = x, y
-
                     k += 3
-                    
 
             def collision_text_updn( collision_merged):   
-                """ this function takes care of labels up-start and down-end"""
+                """this function takes care of labels up-start and down-end lab"""
+                self.canvas.flush_events()
+                """ Variables Declaration"""
                 k = 1
                 previous_x, previous_y= 0, 0
                 label_var = ''
                 y_overlap_both_up = 0
                 y_buffer_both_up = 0
-                # original_x = 0
+                self.canvas.flush_events()
 
                 for i in range(len(collision_merged[0])):
                     label_ = collision_merged[0][i]
                     x = collision_merged[1][i]
                     y = collision_merged[2][i]
 
-                    if abs(x - previous_x) <= 0.06 and y == previous_y:
-                    # if abs(x - previous_x) <= 0.06 and previous_y - y:
+                    if abs(x - previous_x) <= 0.03 and y == previous_y:
                         label_var = '/'
                         len_of_labels = len(label_)
                         y_buffer_both_up = y_buffer_both_up + (extract_current_axes_us_de(x, y) * len_of_labels) 
-                        # y_buffer_both_up = y_buffer_both_up + (0.08 * len_of_labels) + 1.4
-                        """y_overlap_both_up is y axis for text which is different in arrows y axis that is 'y' """
                         y_overlap_both_up = y + y_buffer_both_up 
                     else:
                         label_var = ''
                         y_buffer_both_up = 0
                         y_overlap_both_up = y + y_buffer_both_up                         
 
-                    # label = new_dict['UPDN'][k - 1] + label_var
                     label = label_ + label_var
+
                     inx, iny, x, y = self.add_arrow_labels(x, y)
-                    self.axes[inx][iny].text(x - 0.03, y_overlap_both_up  + 1.3, label, rotation = 'vertical', fontsize=8)  # NOTE: INSIDE IF
-                    self.axes[inx][iny].arrow(x, y, 0, 0.5, width = 0.005, clip_on = False)
+                    self.canvas.flush_events()
+                    self.artist_list.append(self.axes[inx][iny].text(x - 0.02, y_overlap_both_up + 2.7, label, rotation = 'vertical', fontsize=8,picker=True))  # NOTE: INSIDE IF
+                    self.artist_list.append(self.axes[inx][iny].arrow(x, y, 0, 0.5, width = 0.005, clip_on = False))
                     previous_x, previous_y = x, y
                     k += 3
-            
+        
             collision_text_updn(collision_merged)  
             collision_text_updn1(collision_merged1)
-
-            intersection(station_dict, self.axes, trains_dict)
-
+            
+            self.intersection(station_dict, trains_dict)
         plot_labels()
         self.canvas.draw()
