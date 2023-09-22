5e5092619f336ef5ebbbfdec3d3d04d75861ecf4
arpit11814 <arpitsuwashchandramishra@gmail.com>
2023-09-22 21:46:07 +0530

    added new names for functions and variables

---
+++ app.py
@@ -0,0 +1,160 @@
+from PySide2 import QtWidgets
+from PySide2.QtGui import QFont
+from PySide2.QtWidgets import QScrollArea, QMessageBox, QApplication, QProgressBar
+from PySide2.QtCore import QObject, QThread, Signal
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.transforms import Bbox, TransformedBbox
+from qt_material import apply_stylesheet
+from collections import Counter
+from blit_manager import BlitManager
+from preprocessing import excel_to_pandas, select, conversion, conversion_box, box_add_24, add_24_down_up, DuplicateTrainError, WrongStationError, SameLengthError, EmptyListError, WrongTimeFormatError
+from labels import *
+from plotted import plotted_
+from matplotlib.text import Text
+from Draggable import dragged
+
+
+
+class ExportWorker(QObject):
+    update_signal = Signal(int)
+    new_fig = Figure(figsize=(10, 50), tight_layout=True)
+    def __init__(self, file_name, canvas):
+        super().__init__()
+        self.file_name = file_name
+        self.canvas = canvas
+        self.figure = canvas.figure
+        print("Worker initialize")
+    def run(self):
+        print("I am running")
+        try:
+            axes = self.figure.axes
+            extent = [ax.get_tightbbox().transformed(self.figure.dpi_scale_trans.inverted()) for ax in axes]
+            # print(extent)
+            extent = [Bbox([[b.x0,b.y0],[b.x1,b.y1+0.4]]) for b in extent]
+            xmin,xmax = extent[0].x0,extent[0].x1
+            extent[3] = Bbox([[xmin, extent[3].y0],[xmax,extent[3].y1]])
+            print(extent)
+            print("Lets make a pdf")
+            with PdfPages(self.file_name) as pdf:
+                print("3 2 1 Gooo!!")
+                for i, bbox in zip(range(12,101,11),extent):
+                    print(f"i:{i}, bbox:{bbox}")
+                    pdf.savefig(self.figure, bbox_inches=bbox, pad_inches=1)
+                    self.update_signal.emit(i)
+        except Exception as e:
+                print(f"Error raised: {e}")
+
+
+class PlotWindow(QtWidgets.QWidget, plotted_):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.excel_to_pandas = excel_to_pandas
+        self.select = select
+        self.conversion = conversion
+        self.conversion_box = conversion_box
+        self.add_24_down_up = add_24_down_up
+        self.box_add_24 = box_add_24
+        self.add_arrow_labels = add_arrow_labels
+        self.merge_elements = merge_elements
+        self.extract_dn_elem= extract_dn_elem
+        self.extract_up_elem = extract_up_elem
+        self.add_keys= add_keys
+        self.add_lables = add_lables
+        self.figure = Figure(figsize=(10, 50), tight_layout=True)
+        self.y_axis = ["CCG","MEL","CYR","GTR","BCT","MX","PL","PBHD","DDR","MRU","MM","BA","BDTS","KHAR","STC","VLP","ADH","JOS","RMAR","GMN","MDD","KILE","BVI","DIC",'MIRA',"BYR","NIG","BSR","NSP","VR","VTN","SAH","KLV","PLG","UOI","BOR","VGN","DRD","GVD","BRRD","UBR","SJN","BLD","KEB","VAPI","BAGD","UVD","PAD","ATUL","BL","DGI","JRS","BIM","AML","ACL","VDH","HXR","GNST","NVS","MRL","SCH","BHET","UDN","ST"]
+        self.y_labes = ["CCG 0.0","MEL","CYR","GTR","BCT 14.66","MX","PL","PBHD","DDR 10.17","MRU","MM","BA 14.66","BDTS 15.29","KHAR","STC","VLP","ADH 29.32","JOS","RMAR","GMN","MDD","KILE","BVI 33.98","DIC",'MIRA',"BYR 43.11","NIG","BSR 51.78","NSP 55.85","VR 59.98","VTN 68.42","SAH 76.74","KLV 82.55","PLG 90.92","UOI 97.15","BOR 102.8","VGN 111.5","DRD 123.7","GVD 134.8","BRRD 139.0","UBR 144.0","SJN 149.4","BLD 160.9","KEB 165.8","VAPI 172.0","BAGD 179.0","UVD 182.0","PAD 187.7","ATUL 191.0","BL 198.22","DGI 207.21","JRS 212.28","BIM 216.41","AML 221.72","ACL 225.33","VDH 228.87","HXR 232.0","GNST 234.0","NVS 237.33","MRL 245.63","SCH 252.26","BHET 257.3","UDN 262.77","ST 266.78"]    # print(down_up)        self.setWindowTitle("Matplotlib Plot")
+        self.canvas = FigureCanvas(self.figure)
+        self.new_canvas = None
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setWidget(self.canvas)
+        self.progress_bar = QProgressBar()
+        self.progress_bar.hide()
+        self.button = QtWidgets.QPushButton("Load Excel File")
+        self.button.clicked.connect(self.load_file)
+        self.axes = None
+        self.export_button = QtWidgets.QPushButton("Export Plot as PDF")
+        self.export_button.clicked.connect(self.export_plot)
+        self.export_button.setEnabled(False)
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.progress_bar)
+        self.layout.addWidget(self.scroll_area)
+        self.layout.addWidget(self.toolbar)
+        self.layout.addWidget(self.button)  
+        self.layout.addWidget(self.export_button)
+        self.canvas.setMinimumSize(5500, 2500)
+        self.canvas.setMaximumSize(5501, 2501)
+        self.bm = None
+        self.artist_list = []
+        self.pl = plotted_(self.figure,self.y_axis,self.y_labes, self.canvas, self.layout,self.export_button,self.axes, self.scroll_area, self.toolbar)
+        self.plot_trains = self.pl.plot_trains
+        self.dragged = None
+        self.dragged_axes = None
+
+    def load_file(self):
+        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open Excel File", "", "Excel Files (*.xlsx)")
+        if file_name:
+            try:
+                select_flag = False
+                down_up, dwn_upp, color_dict, rect_dict =  self.excel_to_pandas(file_name,self.y_axis)
+                if select_flag:
+                    down_up, dwn_upp = self.select(down_up, dwn_upp)
+                down_up = self.conversion(down_up)
+                rect_dict = self.conversion_box(rect_dict)
+                down_up = self.add_24_down_up(down_up)
+                rect_dict = self.box_add_24(rect_dict)
+                self.figure.clear()
+                self.plot_trains(down_up, dwn_upp, color_dict, rect_dict)
+                self.export_button.setEnabled(True)
+                self.bm = BlitManager(self.canvas, self.pl.artist_list)
+                self.dragged = dragged(self.canvas,self.bm)
+                self.canvas.mpl_connect("pick_event", self.dragged.on_pick_event)
+                # self.canvas.mpl_connect("motion_notify_event", self.on_motion_event)
+                self.canvas.mpl_connect("button_release_event", self.dragged.on_release_event)
+
+            except DuplicateTrainError as e:
+                QMessageBox.critical(self, "Error", str(e))
+            except WrongStationError as e:
+                QMessageBox.critical(self, "Error", str(e))
+            except SameLengthError as e:
+                QMessageBox.critical(self, "Error", str(e))
+            except EmptyListError as e:
+                QMessageBox.critical(self, "Error", str(e))              
+            except WrongTimeFormatError as e:
+                QMessageBox.critical(self, "Error", str(e)) 
+    def export_plot(self):
+        file_name, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Save Plot as PDF", "", "PDF Files (*.pdf)")
+        if file_name:
+            self.bm.canvas.mpl_disconnect(self.bm.cid)
+            self.bm.cid = None
+            self.scroll_area.takeWidget()
+            self.layout.removeWidget(self.toolbar)
+            self.export_button.setEnabled(False)
+            self.progress_bar.show()
+            self.export_worker = ExportWorker(file_name,self.canvas)
+            self.export_thread = QThread()
+            self.export_worker.moveToThread(self.export_thread)
+            self.export_thread.started.connect(self.export_worker.run)
+            self.export_worker.update_signal.connect(self.update_progress_bar)
+            self.export_thread.finished.connect(self.export_thread.deleteLater)
+            self.export_thread.start()
+    def update_progress_bar(self, progress):
+        self.progress_bar.setValue(progress)
+        if progress == 100:
+            self.bm.cid = self.bm.canvas.mpl_connect("draw_event", self.bm.on_draw)
+            self.export_thread.quit()
+            self.export_thread.wait()
+            self.progress_bar.hide()
+            self.progress_bar.setValue(0)
+            self.layout.addWidget(self.toolbar)
+            self.scroll_area.setWidget(self.canvas)
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    apply_stylesheet(app, theme='dark_blue.xml')
+    window = PlotWindow()
+    window.show()
+    app.exec_()
---
+++ plotted.py
@@ -0,0 +1,261 @@
+from PySide2 import QtWidgets
+from PySide2.QtGui import QFont
+from PySide2.QtWidgets import QScrollArea, QMessageBox, QApplication
+from matplotlib.figure import Figure
+from matplotlib import patches
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import numpy as np
+from matplotlib.transforms import Bbox, TransformedBbox
+from qt_material import apply_stylesheet
+import pandas as pd
+from matplotlib.ticker import MultipleLocator
+from matplotlib.ticker import FixedFormatter
+import re
+import copy
+from collections import Counter
+from labels import *
+from intersection import intersection
+
+class plotted_():
+    intersection = intersection
+    def __init__(self, figure, y_axis, y_labes,canvas, layout,export_button,axes, scroll_area, toolbar) -> None:
+        # self.excel_to_pandas = excel_to_pandas
+        # self.select = select
+        # self.conversion = conversion
+        # self.add_24_down_up = add_24_down_up
+        self.add_arrow_labels = add_arrow_labels
+        self.extract_current_axes = extract_current_axes_us_de
+        self.extract_current_axes = extract_current_axes_ue_ds
+        self.merge_elements = merge_elements
+        self.extract_dn_elem= extract_dn_elem
+        self.extract_up_elem = extract_up_elem
+        self.add_keys= add_keys
+        self.add_lables = add_lables
+        self.figure= figure
+        self.y_axis = y_axis
+        self.y_labes = y_labes
+        self.canvas = canvas
+        self.layout = layout
+        self.export_button = export_button
+        self.axes = axes
+        self.scroll_area = scroll_area
+        self.toolbar = toolbar
+        self.artist_list = []
+
+    def plot(self,index_0,index_1,arr,start_sub_y_axis,end_sub_y_axis,ylim_start,ylim_end,xlim_start,xlim_end, color_dict, station_dict, rect_dict):
+        self.axes[index_0][index_1].minorticks_on()
+        # xa_0 = np.linspace(0, 8, 200)
+        xa_0 = np.arange(xlim_start, xlim_end, 0.03333)
+        for arr_2d, color_list in zip(station_dict.values(), color_dict.values()):
+            self.canvas.flush_events()
+            for i in range(0, len(arr_2d), 2):
+                arr_minus = np.array(arr_2d[i+1])
+                self.canvas.flush_events()
+                arr_minus = arr_minus - 24
+                line_color = color_list[int(i/2)]
+                (ln1,) = self.axes[index_0][index_1].plot(arr_minus, arr_2d[i], color=line_color)
+                (ln2,) = self.axes[index_0][index_1].plot(arr_2d[i+1], arr_2d[i], color=line_color)
+                self.artist_list.append(ln1)
+                self.artist_list.append(ln2)
+                self.canvas.flush_events()
+        for i in range(len(self.y_axis)):
+            y_index = self.y_axis[i]
+            self.canvas.flush_events()
+            ya = [y_index] * len(xa_0)
+            self.artist_list.append(self.axes[index_0][index_1].scatter(xa_0, ya, marker=',',color='blue', s=0.3))
+            self.canvas.flush_events()
+
+        for key, box_list in rect_dict.items():
+            if key =="DN":
+                grid = "-----"
+            elif key =="UP":
+                grid = "|||"
+            for station, start_time, end_time in box_list:
+                xmin = start_time
+                xmax = end_time
+                ymax = station - 0.15
+                ymin = station - 0.85
+                rect = patches.Rectangle((xmin, ymin), width=xmax-xmin, height= ymax-ymin, alpha=0.6, edgecolor='m', hatch=grid, facecolor='none')
+                self.artist_list.append(self.axes[index_0][index_1].add_patch(rect))
+
+        self.axes[index_0][index_1].xaxis.grid(True, which='major', linestyle='-', color='black')
+        self.axes[index_0][index_1].xaxis.grid(True, which='minor', linestyle='-')
+        self.axes[index_0][index_1].xaxis.set_minor_locator(MultipleLocator(10 / 60))
+        self.axes[index_0][index_1].set_xticks(arr)
+        self.axes[index_0][index_1].set_xticklabels(arr)
+        sub_y_axis = self.y_labes[start_sub_y_axis:end_sub_y_axis]
+        # sub_y_axis = Reverse(sub_y_axis)
+        # print(sub_y_axis)
+        self.axes[index_0][index_1].set_ylim(start_sub_y_axis,end_sub_y_axis)
+        # print(start_sub_y_axis,end_sub_y_axis)
+        # print(range(start_sub_y_axis,end_sub_y_axis))
+        # self.axes[index_0][index_1].set_yticks(range(start_sub_y_axis,end_sub_y_axis))
+        self.axes[index_0][index_1].set_yticks(range(start_sub_y_axis,end_sub_y_axis))
+        self.axes[index_0][index_1].set_yticklabels(sub_y_axis)
+        self.canvas.flush_events()
+
+
+        self.axes[index_0][index_1].tick_params(axis='x', which='minor', labelbottom=True)
+        self.axes[index_0][index_1].tick_params(labeltop=True, labelright=True)
+        minor_labels = ["10", "20", "30", "40", "50"] * 8
+        minor_labels.insert(0, "")
+        self.canvas.flush_events()
+
+
+        formatter = FixedFormatter(minor_labels)
+        self.axes[index_0][index_1].xaxis.set_minor_formatter(formatter)
+        self.axes[index_0][index_1].tick_params(axis='x', which='minor', labelsize=6)
+        self.axes[index_0][index_1].set_xlim(xlim_start,xlim_end)  
+        self.axes[index_0][index_1].invert_yaxis()
+        self.canvas.flush_events()
+
+
+    def plot_trains(self, station_dict, trains_dict, color_dict, rect_dict):
+        self.axes = self.figure.subplots(nrows=3, ncols=3)
+        # have to make  3 x 3 grid
+        # set width of each subplot as 8
+        for key, arr_2d in station_dict.items():
+            self.canvas.flush_events()
+            
+            for i in range(0, len(arr_2d), 2):
+                self.canvas.flush_events()
+
+                for j in range(len(arr_2d[i])):
+                    arr_2d[i][j] = self.y_axis.index(arr_2d[i][j])
+                    self.canvas.flush_events()
+
+        # dup_train_dict = copy.deepcopy(trains_dict)
+        self.canvas.flush_events()
+
+
+        for key, box_list in rect_dict.items():
+            for i, (station, *_) in enumerate(box_list):
+                station = self.y_axis.index(station)
+                box_list[i] = station,*_,
+
+    ######################################################################################################    
+        arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        arr2 = [8, 9, 10, 11, 12, 13, 14, 15, 16]
+        arr3 = [16, 17, 18, 19, 20, 21, 22, 23, 24]
+        
+    #   (index_0, index_1, arr, start_sub_y_axis, end_sub_y_axis, ylim_start, ylim_end, xlim_start, xlim_end)
+        self.plot(0,0,arr1,0,30,      0,15,        0,8, color_dict, station_dict, rect_dict)
+        self.plot(1,0,arr1,29,50,    0,19,        0,8, color_dict, station_dict, rect_dict)   
+        self.plot(2,0,arr1,49,64,    0,19,        0,8, color_dict, station_dict, rect_dict) 
+
+        self.plot(0,1,arr2,0,30,      0,15,        8,16, color_dict,   station_dict, rect_dict)
+        self.plot(1,1,arr2,29,50,    0,19,       8,16,  color_dict,  station_dict, rect_dict)
+        self.plot(2,1,arr2,49,64,    0,19,       8,16,  color_dict,  station_dict, rect_dict)
+
+        self.plot(0,2,arr3,0,30,      0,15,       16,24,  color_dict,  station_dict, rect_dict)
+        self.plot(1,2,arr3,29,50,    0,19,       16,24,  color_dict,  station_dict, rect_dict)
+        self.plot(2,2,arr3,49,64,    0,19,        16,24,  color_dict,  station_dict, rect_dict)
+
+        def plot_labels():
+            new_dict = copy.deepcopy(station_dict)
+
+            """ Called a func to add lebels in dictionary"""
+            new_dict = self.add_lables(new_dict, trains_dict)
+            
+            """ Called a func to add keys in dictionary"""
+            new_dict = self.add_keys(new_dict)
+            
+            new_dict['UPDN'] = new_dict['UP'] + new_dict['DN']
+
+            """ Called a func that extract up start and up end elements"""
+            upStart, upEnd = self.extract_up_elem(new_dict)  
+
+            """ Called a func that extract dn start and dn end elements"""        
+            dnStart, dnEnd = self.extract_dn_elem(new_dict)
+
+            """ Called a func that colab up start and dn end elements"""
+            upStart_dnEnd = self.merge_elements(upStart, dnEnd)
+
+            """ Called a func that colab dn start and up end elements"""        
+            upEnd_dnStart = self.merge_elements(dnStart, upEnd)
+
+            """ Called a func that colab up start and up end elements for arrows """
+            upStart_upEnd = self.merge_elements(upStart, upEnd)
+
+            """ Called a func that colab dn start and dn end elements for arrows """
+            dnStart_dnEnd = self.merge_elements(dnStart, dnEnd)
+
+########################################## collision text for up and down #################################################\
+            
+            def upEnd_dnStart_label(upEnd_dnStart):
+                """ this function takes care of labels up-end and down-start labels and arrows"""
+                self.canvas.flush_events()
+                k = 1
+                previous_x, previous_y= 0, 0
+                label_var = ''
+                final_y = 0
+                y_buffer = 0
+
+                for i in range(len(upEnd_dnStart[0])):
+                    self.canvas.flush_events()
+
+                    label_ = upEnd_dnStart[0][i]
+                    x = upEnd_dnStart[1][i]
+                    y = upEnd_dnStart[2][i]
+                    if abs(x - previous_x) <=0.03 and y == previous_y:
+                        label_var = '/'
+                        len_of_labels = len(label_)
+                        slash_buffer = extract_current_axes_ue_ds(x, y)
+                        y_buffer = y_buffer + (slash_buffer * len_of_labels) 
+                        final_y = y - y_buffer 
+                    else:
+                        label_var = ''
+                        y_buffer = 0
+                        final_y = y - y_buffer                         
+
+                    label = label_var + label_
+
+                    inx, iny, x, y = self.add_arrow_labels(x, y)
+                    self.artist_list.append(self.axes[inx][iny].text(x - 0.02, final_y - 0.9, label, rotation = 'vertical', fontsize=8,picker=True))
+                    self.artist_list.append(self.axes[inx][iny].arrow(x, y, 0, - 0.5, width = 0.005, clip_on = False))
+                    previous_x, previous_y = x, y
+                    k += 3
+
+            def upStart_dnEnd_label( upStart_dnEnd):   
+                """this function takes care of labels up-start and down-end lab"""
+                self.canvas.flush_events()
+                k = 1
+                previous_x, previous_y= 0, 0
+                label_var = ''
+                final_y = 0
+                y_buffer = 0
+                self.canvas.flush_events()
+
+                for i in range(len(upStart_dnEnd[0])):
+                    label_ = upStart_dnEnd[0][i]
+                    x = upStart_dnEnd[1][i]
+                    y = upStart_dnEnd[2][i]
+
+                    if abs(x - previous_x) <= 0.03 and y == previous_y:
+                        label_var = '/'
+                        len_of_labels = len(label_)
+                        y_buffer = y_buffer + (extract_current_axes_us_de(x, y) * len_of_labels) 
+                        final_y = y + y_buffer 
+                    else:
+                        label_var = ''
+                        y_buffer = 0
+                        final_y = y + y_buffer                         
+
+                    label = label_ + label_var
+
+                    inx, iny, x, y = self.add_arrow_labels(x, y)
+                    self.canvas.flush_events()
+                    self.artist_list.append(self.axes[inx][iny].text(x - 0.02, final_y + 1.4, label, rotation = 'vertical', fontsize=8, picker=True))  # NOTE: INSIDE IF
+                    self.artist_list.append(self.axes[inx][iny].arrow(x, y, 0, 0.5, width = 0.005, clip_on = False))
+                    previous_x, previous_y = x, y
+                    k += 3
+        
+            upStart_dnEnd_label(upStart_dnEnd)  
+            upEnd_dnStart_label(upEnd_dnStart)
+            
+            self.intersection(station_dict, trains_dict)
+        plot_labels()
+        self.canvas.draw()
